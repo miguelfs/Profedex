11e5543a6ffc02a5da7f696a7ebfa6b996626547
Miguel <mfsousa@poli.ufrj.br>
2016-06-14 22:26:57 -0300

    Added a Navigation Drawer and a button to add teachers

--- app/src/main/java/com/partiufast/profedex/Activities/OakActivity.java
+++ app/src/main/java/com/partiufast/profedex/Activities/OakActivity.java
@@ -6,6 +6,7 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.partiufast.profedex.R;
@@ -14,6 +15,7 @@ import com.partiufast.profedex.Typewriter;
 public class OakActivity extends AppCompatActivity {
     Typewriter textView;
     ImageView oakImageView;
+    Button mNextOakButton, mJumpOakButton;
     int index = 0;
     MediaPlayer mp;
 
@@ -23,6 +25,8 @@ public class OakActivity extends AppCompatActivity {
         setContentView(R.layout.activity_oak);
         textView = (Typewriter) findViewById(R.id.oak_text_view);
         oakImageView = (ImageView) findViewById(R.id.oak_image_view);
+        mNextOakButton = (Button) findViewById(R.id.nextOakButton);
+        mJumpOakButton = (Button) findViewById(R.id.jumpOakButton);
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/pkmn.ttf");
         textView.setTypeface(font);
 
@@ -31,39 +35,41 @@ public class OakActivity extends AppCompatActivity {
 
         textView.setCharacterDelay(50);
         textView.animateText(getResources().getString(R.string.oak_text1));
-        oakImageView.setOnClickListener(new View.OnClickListener() {
+        mJumpOakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OakActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        mNextOakButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch (index) {
-                    case 1:
+                    case 0:
                         textView.animateText(getResources().getString(R.string.oak_text2));
                         index++;
                         break;
-                    case 2:
+                    case 1:
                         textView.animateText(getResources().getString(R.string.oak_text3));
                         index++;
                         break;
-                    case 3:
+                    case 2:
                         textView.animateText(getResources().getString(R.string.oak_text4));
                         index++;
                         break;
-                    case 4:
+                    case 3:
                         textView.animateText(getResources().getString(R.string.oak_text5));
                         index++;
                         break;
-                    case 5:
+                    case 4:
                         textView.animateText(getResources().getString(R.string.oak_text6));
                         index++;
                         break;
-                    case 6:
+                    case 5:
                         textView.animateText(getResources().getString(R.string.oak_text7));
+                        mNextOakButton.setVisibility(View.GONE);
                         index++;
-                        break;
-                    case 7:
-                        Intent intent = new Intent(OakActivity.this, ListActivity.class);
-                        mp.stop();
-                        startActivity(intent);
-                        finish();
                         break;
                     default:
                         index++;
