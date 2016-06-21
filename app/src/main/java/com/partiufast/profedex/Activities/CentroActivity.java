1f648d58e8d29556035c8ba328498507c44369ae
Carol <karolmenezes@msn.com>
2016-06-21 10:38:51 -0300

    Lista de Centros Criada

--- app/src/main/java/com/partiufast/profedex/Activities/CentroActivity.java
+++ app/src/main/java/com/partiufast/profedex/Activities/CentroActivity.java
@@ -25,14 +25,6 @@ public class CentroActivity extends AppCompatActivity {
 
         universityListView();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     private void universityListView() {
