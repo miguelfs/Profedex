193fe00a4713b138a9a60812c43ee33a607e1897
lgos <lgos@poli.ufrj.br>
2016-06-23 10:16:41 -0300

    IP fix

--- app/src/main/java/com/partiufast/profedex/app/AppConfig.java
+++ app/src/main/java/com/partiufast/profedex/app/AppConfig.java
@@ -2,8 +2,8 @@ package com.partiufast.profedex.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "http://10.0.0.101:1234/login.php";
+	public static String URL_LOGIN = "http://10.0.2.2:1234/login.php";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://10.0.0.101:1234/register.php";
+	public static String URL_REGISTER = "http://10.0.2.2:1234/register.php";
 }
